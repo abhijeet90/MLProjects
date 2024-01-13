4d7e8e5282e8db83aa8a9ce5bb99c4e67f190a91
abhijeet90 <kadam.abhijeet90@gmail.com>
2024-01-13 14:52:41 +0530

    'data_injestion'

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -12,7 +12,7 @@ from src.components.data_transformation import DataTransformationConfig
 
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
-@dataclass
+@dataclass  
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
